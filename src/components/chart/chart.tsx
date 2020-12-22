8fdea70e7d702ba80d2b17a54f6b07c6b4be256a
Kadamir24 <amir.kaniyev@gmail.com>
2020-12-22 15:59:00 +0600

    feat: add prototype for display chart of specific country

--- src/components/chart/chart.tsx
+++ src/components/chart/chart.tsx
@@ -2,17 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios';
 
-// async function fetchDate(url) {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     return data;
-// }
-
-// const getData = async () => {
-//     const data = await fetchDate('https://disease.sh/v3/covid-19/historical/all?lastdays=340');
-//     return data;
-// };
-
 const fetchData = async () => {
     const { data } = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=340');
     console.log('DATA CHE TAM', data);
@@ -21,11 +10,24 @@ const fetchData = async () => {
         deaths: dataClass.deaths,
         recovered: dataClass.recovered,
     }));
+    console.log('NEWDATA', newData);
+    return newData;
+};
 
+const fetchDataByCountry = async () => {
+    const { data } = await axios.get('https://disease.sh/v3/covid-19/historical/Russia?lastdays=340');
+    console.log('DATA CHE TAM', data);
+    const newData = [data].map((dataClass) => ({
+        cases: dataClass.timeline.cases,
+        deaths: dataClass.timeline.deaths,
+        recovered: dataClass.timeline.recovered,
+    }));
+    console.log('NEWDATA', newData);
     return newData;
 };
 
 const Chart = () => {
+    const flag = true;
     const [items, setItems] = useState({});
 
     // useEffect(() => {
@@ -35,40 +37,43 @@ const Chart = () => {
     // }, []);
     useEffect(() => {
         const fetchMyAPI = async () => {
-            const initData = await fetchData();
+            const initData = flag ? await fetchData() : await fetchDataByCountry();
             setItems(initData);
         };
         fetchMyAPI();
     }, []);
-    console.log('chetam', items[0]);
-    // console.log('checkcheck23123', Object.keys(items[0]));
-    // const data = items.map(({ cases }) => cases);
-    // console.log('DATA', data);
-    // console.log('checkcheck', Object.keys(items[0]));
-    if (items[0]) {
-        console.log('ifif', Object.keys(items[0].cases));
-    }
+    // console.log('chetam', items[0]);
+    // // console.log('checkcheck23123', Object.keys(items[0]));
+    // // const data = items.map(({ cases }) => cases);
+    // // console.log('DATA', data);
+    // // console.log('checkcheck', Object.keys(items[0]));
+    // // let gettingData;
+    // if (items[0]) {
+    //     console.log('ifif', Object.keys(items[0].cases));
+    // }
+    const gettingData = items[0];
+    // console.log('gettingData', gettingData);
 
     return (
-        items[0] ? (
+        gettingData ? (
             <div>
                 <Line
                     data={{
-                        labels: Object.keys(items[0].cases).map((item) => item),
+                        labels: Object.keys(gettingData.cases).map((item) => item),
                         // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                         datasets: [{
                             label: 'cases',
-                            data: Object.values(items[0].cases).map((item) => item),
+                            data: Object.values(gettingData.cases).map((item) => item),
                             borderColor: '#3333ff',
                             fill: true,
                         }, {
                             label: 'deaths',
-                            data: Object.values(items[0].deaths).map((item) => item),
+                            data: Object.values(gettingData.deaths).map((item) => item),
                             borderColor: 'red',
                             fill: true,
                         }, {
                             label: 'recovered',
-                            data: Object.values(items[0].recovered).map((item) => item),
+                            data: Object.values(gettingData.recovered).map((item) => item),
                             borderColor: 'green',
                             fill: true,
                         }],
@@ -90,67 +95,5 @@ const Chart = () => {
         ) : null
     );
 };
-
-// const Chart = () => {
-//     // const [date, setDate] = useState([]);
-//     // let labels;
-//     // let data = [];
-//     // labels = Object.keys(items.cases).map((element) => element);
-//     // let data = {};
-//     // const data = fetchDate('https://disease.sh/v3/covid-19/historical/all?lastdays=340')
-//     // // .then((items) => items);
-//     // console.log(data);
-//     // let data;
-//     // const data = fetchDate('https://disease.sh/v3/covid-19/historical/all?lastdays=340');
-//     //     .then((items) => { data = items; });
-//     // console.log(data);
-//     const data = getData();
-//     console.log(data);
-
-//     return (
-//         <div>
-//             <Line
-//                 data={{
-//                     // labels: data.map((items) => items.cases),
-//                     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//                     // labels: Object.keys(data.cases),
-//                     datasets: [{
-//                         label: '# of Votes',
-//                         data: [10, 11, 12, 13, 14, 5],
-//                         backgroundColor: [
-//                             'rgba(255, 99, 132, 0.2)',
-//                             'rgba(54, 162, 235, 0.2)',
-//                             'rgba(255, 206, 86, 0.2)',
-//                             'rgba(75, 192, 192, 0.2)',
-//                             'rgba(153, 102, 255, 0.2)',
-//                             'rgba(255, 159, 64, 0.2)',
-//                         ],
-//                         borderColor: [
-//                             'rgba(255, 99, 132, 1)',
-//                             'rgba(54, 162, 235, 1)',
-//                             'rgba(255, 206, 86, 1)',
-//                             'rgba(75, 192, 192, 1)',
-//                             'rgba(153, 102, 255, 1)',
-//                             'rgba(255, 159, 64, 1)',
-//                         ],
-//                         borderWidth: 1,
-//                     }],
-//                 }}
-//                 height={300}
-//                 width={600}
-//                 options={{
-//                     maintainAspectRatio: false,
-//                     scales: {
-//                         yAxes: [{
-//                             ticks: {
-//                                 beginAtZero: true,
-//                             },
-//                         }],
-//                     },
-//                 }}
-//             />
-//         </div>
-//     );
-// };
 
 export default Chart;
